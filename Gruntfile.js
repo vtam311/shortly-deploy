a09c329033ce63f1117145e685cebcf74335a7df
Hack Reactor Students <accounts@hackreactor.com>
2015-04-24 17:02:19 -0700

    Created production js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,14 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
+      dist: {
+        src: [
+          './public/client/*.js',
+        ],
+        dest: "./public/dist/production.js",
+      }
     },
 
     mochaTest: {
